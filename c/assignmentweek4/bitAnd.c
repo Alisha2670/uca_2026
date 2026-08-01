bd74d71255a5072ff3fb7f556bfd9d5fd0a51489
Alisha Garg <alisha2670.beai24@chitkara.edu.in>
2026-08-01 09:39:35 +0000

    bitAnd

---
+++ c/assignmentweek4/bitAnd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int bitAnd(int x, int y) {
+    return ~(~x | ~y);
+}
+
+int main() {
+    int x, y;
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+    printf("bitAnd = %d\n", bitAnd(x, y));
+    return 0;
+}
