1c4b089a5330a2f030822e3cbb4230377c9d9fd8
Alisha Garg <alisha2670.beai24@chitkara.edu.in>
2026-08-01 09:41:33 +0000

    fitBits

---
+++ c/assignmentweek4/fitBits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fitsBits(int x, int n) {
+    int shift = 32 + (~n + 1);  
+    return !(((x << shift) >> shift) ^ x);
+}
+
+int main() {
+    int x, n;
+    printf("Enter value of x and n:");
+    scanf("%d %d", &x, &n);
+    printf("%d\n", fitsBits(x, n));
+    return 0;
+}
