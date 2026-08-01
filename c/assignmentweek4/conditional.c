aaeada989aea24a8a5a4d1b89b698238d55b270a
Alisha Garg <alisha2670.beai24@chitkara.edu.in>
2026-08-01 09:40:45 +0000

    conditional

---
+++ c/assignmentweek4/conditional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int conditional(int x, int y, int z) {
+    int mask = ~(!x) + 1;
+    return (~mask & y) | (mask & z);
+}
+
+int main() {
+    int x, y, z;
+    printf("Enter x, y and z: ");
+    scanf("%d %d %d", &x, &y, &z);
+    printf("%d\n", conditional(x, y, z));
+    return 0;
+}
